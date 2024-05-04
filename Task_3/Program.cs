f08442f04befc2aa2937bb209aca05037532fee7
Danil <eliseev.danii@yandex.ru>
2024-05-04 17:51:24 +0300

    add lesson2:task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿//int n = 10;
+int[] array = { 1, 3, 4, 5, 7, 9, 12 };
+int i = 0;
+
+while (i < array.Length)
+{
+    if (array[i] % 2 == 0)
+    {  
+        Console.Write($"{array[i]} ");
+        i = i++;
+    }
+    i = i+1;
+}
