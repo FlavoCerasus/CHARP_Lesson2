1cc9507fac4b295aacb0175397caa74a5c6ef5d0
Danil <eliseev.danii@yandex.ru>
2024-05-04 18:13:32 +0300

    add lesson2:task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,34 @@
+﻿int n = 5;
+int [] array = {2,4,7,3,9};
+int i = 0;
+int max = array[0];
+
+while (i<n)
+{
+    if (array[i]>max)
+    {
+        max = array[i];
+    }  
+    i = i+1;
+}
+Console.Write(max);
+
+max = array[0];
+for (int j = 0; j<array.Length; j++)
+{
+    if (array[j]>max)
+    {
+        max = array[j];
+    }  
+}
+Console.Write(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e>max)
+    {
+        max = e;
+    }
+}
+Console.Write(max);
